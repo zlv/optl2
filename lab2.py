ea952946b27f882acbef60b1e1b4aa46739daef9
Evgenii Lezhnin <zlvlezhnin@gmail.com>
2014-03-15 00:06:31 +0700

    basic code

--- lab2.py
+++ lab2.py
@@ -1,100 +1,36 @@
 from math import *
 
 class Poly :
-	coef = []
-	def eval(x, iDerivative=0) :
-		sum = 0
-		for i in range(0, len(coef)-iDerivative) :
-			sum += coef[i+iDerivative]*find_der_mult(i+iDerivative,iDerivative)*(x^i)
-	def find_der_mult(ix,iDerivative) :
-		if iDerivative==0 :
-			return 1
-		return ix*find_der_mult(ix-1,iDerivative-1)
+    coef = []
+    def __init__(self, coef0) :
+        self.coef = coef0
 
-def eval(x, i=0) :
-    if i==0 :
-        return 3*x*x-15*x
-    elif i==1 :
-        return 6*x-15
+    def eval(self, x, iDerivative=0) :
+        sum = 0
+        for i in range(0, len(self.coef)-iDerivative) :
+            sum += self.coef[i+iDerivative]*self.find_der_mult(i+iDerivative,iDerivative)*(x^i)
 
-def brute_force(a, b, n, minx, miny) :
-    print('brute_force:')
-    inc=(b-a)/(n+1)
-    minx[0] = a
-    miny[0] = eval(a);
-    x=a+inc
-    while (x<=b) :
-        y = eval(x)
-        if y<miny[0] :
-            minx[0] = x
-            miny[0] = y
-        x += inc
+    def find_der_mult(self, ix, iDerivative) :
+        if iDerivative==0 :
+            return 1
+        return ix*self.find_der_mult(ix-1,iDerivative-1)
 
-def diho(a, b, eps, minx, miny) :
-    print('dihotomy:')
-    while (b-a>eps) :
-        c = (a+b) / 2
-        if eval(b) > eval(a) :
-            b = c
+def Bolcano(a, b, poly, eps, x, y) :
+    x[0] = (a+b)/2
+    y[0] = poly.eval(x[0])
+    while abs(y[0])>eps :
+        if y[0]>0 :
+            b = x[0]
         else :
-            a = c
-    minx[0] = (a+b)/2
-    miny[0] = eval(minx[0]);
+            a = x[0]
+        x[0] = (a+b)/2
+        y[0] = poly.eval(x[0])
 
-def golden(a, b, eps, minx, miny) :
-    print('golden ratio:')
-    while (b-a>eps) :
-        ratio = (b-a)/((sqrt(5)+1)/2)
-        x1 = b-ratio
-        x2 = a+ratio
-        if eval(x1) > eval(x2) :
-            a = x1
-        else :
-            b = x2
-    minx[0] = (a+b)/2
-    miny[0] = eval(minx[0]);
-
-fibo_arr = [0,1]
-def fibonacci(n) :
-    return fibo_arr[n]
-
-def init_fibo(n) :
-    for i in range(2,n+1) :
-        fibo_arr.append(fibonacci(i-1)+fibonacci(i-2))
-
-def fibo(a, b, n, minx, miny) :
-    print('Fibonacci:')
-    x1 = a+(b-a)*fibonacci(n-2)/fibonacci(n)
-    x2 = a+(b-a)*fibonacci(n-1)/fibonacci(n)
-    y1 = eval(x1)
-    y2 = eval(x2)
-    while (n>1) :
-        if y1 > y2 :
-            a = x1
-            x1 = x2
-            minx[0] = x1
-            x2 = b-(x1-a)
-            y1 = y2
-            y2 = eval(x2)
-        else :
-            b = x2
-            x2 = x1
-            minx[0] = x2
-            x1 = a+(b-x2)
-            y2 = y1
-            y1 = eval(x1)
-        n-=1
-    miny[0] = eval(minx[0]);
-
-x = [0.]
-y = [0.]
-brute_force(0., 10., 10000, x, y)
-print(x)
-diho(0., 10., 1e-5, x, y)
-print(x)
-golden(0., 10., 1e-5, x, y)
-print(x)
-n = 1000
-init_fibo(n)
-fibo(0., 10., n, x, y)
+a = 0
+b = 10
+poly = Poly([0,-12,2])
+eps = 1e-5
+x = [0]
+y = [0]
+Bolcano(a,b,poly,eps,x,y)
 print(x)
